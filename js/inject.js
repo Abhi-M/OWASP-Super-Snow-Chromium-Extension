d881aaf61452b7487f0453dd51478eb4dade33c7
Abhi-M <abhimbalakrishnan@gmail.com>
2022-01-20 19:08:16 -0800

    Open sourcing
    Initial release

---
+++ js/inject.js
@@ -0,0 +1,15 @@
+if( extension_status == 'on' ) {
+    var all = document.body.getElementsByTagName("*");
+var css = document.createElement("style");
+css.innerHTML = "* { -webkit-filter: blur(2px); } *:hover { -webkit-filter: blur(0px); }'";
+css.type = "text/css";
+document.body.appendChild(css);
+chrome.browserAction.setIcon({ path: 'icons/snow-on.png'});
+}
+if( extension_status == 'off' ) {
+    var all = document.body.getElementsByTagName("*");
+var css = document.createElement("style");
+css.innerHTML = "* { -webkit-filter: blur(0px); } *:hover { -webkit-filter: blur(0px); }'";
+css.type = "text/css";
+document.body.appendChild(css);
+}
